fe2847c2197c8186dcca19d0b0d4c597e8311f26
Alejandro Cuartas <cuartasmarin734@gmail.com>
2021-12-20 12:53:57 -0500

    url update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ const Navbar = () => {
     return(
         <nav className="navbar navbar-light bg-light">
             <form className="container-fluid justify-content-start">
-                <Link to='/' className="btn btn-primary me-2" type="button">Sessions</Link>
-                <Link to='/info' className="btn btn-sm btn-info me-2" type="button">Info</Link>
-                <Link to='/docs' className='btn btn-dark btn-sm'>docs</Link>
+                <Link to='/post-something-frontend/' className="btn btn-primary me-2" type="button">Sessions</Link>
+                <Link to='/post-something-frontend/info' className="btn btn-sm btn-info me-2" type="button">Info</Link>
+                <Link to='/post-something-frontend/docs' className='btn btn-dark btn-sm'>docs</Link>
             </form>
         </nav>
     )
